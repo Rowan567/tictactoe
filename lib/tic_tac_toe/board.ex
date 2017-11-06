d6ba05bb3df92847f20b2f1776ae532773618ea0
Rowan Duguid <rowanduguid@gmail.com>
2017-11-06 19:34:17 +0000

    Make the computer try to win the game. Mainly just defensively and random moves. Not too hard to play against

--- lib/tic_tac_toe/board.ex
+++ lib/tic_tac_toe/board.ex
@@ -39,9 +39,105 @@ defmodule TicTacToe.Board do
     """
     def computer_move(board, type) do
 
-        # If no positions just picka a random move
-        xpos = Enum.random(["0","1","2"])
-        ypos = Enum.random(["0","1","2"])
+        if (type == "X") do
+            player_type = "O"
+        else 
+            player_type = "X"
+        end
+        
+
+        case can_win(board) do
+            {true, xpos, ypos} ->
+                move(board, xpos, ypos, type)
+            false ->
+                computer_move_no_win(board, type)
+        end
+    end
+
+    defp can_win(%{"0" => row1, "1" => row2, "2" => row3}) do
+        %{"0" => cell00, "1" => cell01, "2" => cell02} = row1
+        %{"0" => cell10, "1" => cell11, "2" => cell12} = row2
+        %{"0" => cell20, "1" => cell21, "2" => cell22} = row3
+
+        cond do
+            # Horizontal winners
+            cell01 == cell02 and cell01 != :empty and cell00 == :empty ->
+                {true, "0", "0"}
+            cell00 == cell02 and cell00 != :empty and cell01 == :empty ->
+                {true, "1", "0"}
+            cell00 == cell01 and cell00 != :empty and cell02 == :empty ->
+                {true, "2", "0"}
+            cell11 == cell12 and cell11 != :empty and cell10 == :empty ->
+                {true, "0", "1"}
+            cell10 == cell12 and cell10 != :empty and cell11 == :empty ->
+                {true, "1", "1"}
+            cell10 == cell11 and cell10 != :empty and cell12 == :empty ->
+                {true, "2", "1"}
+            cell21 == cell22 and cell21 != :empty and cell20 == :empty ->
+                {true, "0", "2"}
+            cell20 == cell22 and cell20 != :empty and cell21 == :empty ->
+                {true, "1", "2"}
+            cell20 == cell21 and cell20 != :empty and cell22 == :empty ->
+                {true, "2", "2"}
+            # Vertial winners
+            cell10 == cell20 and cell10 != :empty and cell00 == :empty ->
+                {true, "0", "0"}
+            cell00 == cell20 and cell00 != :empty and cell10 == :empty ->
+                {true, "0", "1"}
+            cell00 == cell10 and cell00 != :empty and cell20 == :empty ->
+                {true, "0", "2"}
+            cell11 == cell21 and cell11 != :empty and cell01 == :empty ->
+                {true, "1", "0"}
+            cell01 == cell21 and cell01 != :empty and cell11 == :empty ->
+                {true, "1", "1"}
+            cell01 == cell11 and cell01 != :empty and cell21 == :empty ->
+                {true, "1", "2"}
+            cell12 == cell22 and cell12 != :empty and cell02 == :empty ->
+                {true, "2", "0"}
+            cell02 == cell22 and cell02 != :empty and cell12 == :empty ->
+                {true, "2", "1"}
+            cell02 == cell12 and cell02 != :empty and cell22 == :empty ->
+                {true, "2", "2"}
+            # Diagonal winners
+            cell11 == cell22 and cell11 != :empty and cell00 == :empty ->
+                {true, "0", "0"}
+            cell00 == cell22 and cell00 != :empty and cell11 == :empty ->
+                {true, "1", "1"}
+            cell00 == cell11 and cell00 != :empty and cell22 == :empty ->
+                {true, "2", "2"}
+            cell20 == cell11 and cell20 != :empty and cell02 == :empty ->
+                {true, "2", "0"}
+            cell02 == cell20 and cell02 != :empty and cell11 == :empty ->
+                {true, "1", "1"}
+            cell02 == cell11 and cell02 != :empty and cell20 == :empty ->
+                {true, "0", "2"}
+            # catch all to return false meaning computer will attempt another move
+            true ->
+                false
+        end
+    end
+
+    defp computer_move_no_win(board = %{"0" => row1, "2" => row3}, type) do
+        %{"0" => cell00, "2" => cell02} = row1
+        %{"0" => cell20, "2" => cell22} = row3
+        
+        cond do
+            cell00 == :empty ->
+                move(board, "0", "0", type)
+            cell02 == :empty ->
+                move(board, "2", "0", type)
+            cell20 == :empty ->
+                move(board, "0", "2", type)
+            cell22 == :empty ->
+                move(board, "2", "2", type)
+            true ->
+                random_move(board, type)
+        end
+    end
+
+    defp random_move(board, type) do
+        xpos = Enum.random(["0", "1", "2"])
+        ypos = Enum.random(["0", "1", "2"])
         case move(board, xpos, ypos, type) do
             {:ok, board} ->
                 {:ok, board}
@@ -52,19 +148,6 @@ defmodule TicTacToe.Board do
                 computer_move(board, type)
         end
     end
-
-    # Checks if the computer has any current positions on the board
-    # defp positions_of(board, type) do
-    #     Map.to_list(board)
-    #     |> Enum.any?(
-    #         fn({_, row}) ->
-    #             Map.to_list(row)
-    #             |> Enum.any?(
-    #                 fn({_, cell}) ->
-    #                     cell == type
-    #                 end)              
-    #         end)
-    # end
 
     # Pricate function to set the value in the board map
     defp set_value(board, xpos, ypos, value) do
